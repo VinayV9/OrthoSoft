f7a22b97c508b96873384d4e3b3497141501e39c
VinayV9 <vinayveeren@gmail.com>
2018-05-20 00:49:44 +0530

    side nav added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SkeletonComponent } from './components/skeleton/skeleton.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'skeleton',
+    component: SkeletonComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
