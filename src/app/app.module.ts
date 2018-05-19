84bb0f79512f001f738f3e5402567b0cc00993fe
VinayV9 <vinayveeren@gmail.com>
2018-05-20 00:48:59 +0530

    ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialdesignModule } from './modules/material-design/material-design.module';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { SkeletonComponent } from './components/skeleton/skeleton.component';
 import { DisplayPartsComponent } from './components/display-parts/display-parts.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SkeletonComponent,
-    DisplayPartsComponent
+    DisplayPartsComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialdesignModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
