e0afabd303bea22b2922aed893c1d1a9111d63b3
VinayV9 <vinayveeren@gmail.com>
2018-05-31 02:25:06 +0530

    patient eoute add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SkeletonComponent } from './components/skeleton/skeleton.component';
+import { PatientComponent } from './components/patient/patient.component';
 
 const routes: Routes = [
   {
     path: 'skeleton',
     component: SkeletonComponent
+  },
+  {
+    path: 'patients',
+    component: PatientComponent
   }
 ];
 
