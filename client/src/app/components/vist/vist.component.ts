5a32554345390d12134bf925f394cf4866774085
VinayV9 <vinayveeren@gmail.com>
2018-06-23 18:47:06 +0530

    visit component created

--- client/src/app/components/vist/vist.component.ts
+++ client/src/app/components/vist/vist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Visit } from '../../models/user';
 
 @Component({
   selector: 'app-vist',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vist.component.scss']
 })
 export class VistComponent implements OnInit {
-
+  public visit: Visit = new Visit();
   constructor() { }
 
   ngOnInit() {
+  }
+  /**
+   * when a patient comes for a visit
+   * register desk takes the problem description
+   */
+  registerVisit(){
+     console.log(this.visit);
   }
 
 }
